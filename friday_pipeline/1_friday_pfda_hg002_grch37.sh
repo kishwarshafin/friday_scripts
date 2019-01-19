dc62b2f91ce440627acf246967fa8db711b529ad
Kishwar <kishwar.shafin@gmail.com>
2019-01-19 12:26:47 -0800

    evaluation pipeline :fire:

--- friday_pipeline/1_friday_pfda_hg002_grch37.sh
+++ friday_pipeline/1_friday_pfda_hg002_grch37.sh
@@ -51,24 +51,3 @@ time python3 call_variants.py \
 --output_dir ${VCF_OUTPUT_DIR}
 
 wait;
-
-
-# FOR FRIDAY THIS IS DONE SEPARATELY, ONCE THE FINAL OUTPUT IS GENREATED THE FINAL OUTPUT FILE NEEDS TO GO THERE
-# HAP.PY
-sudo docker pull pkrusche/hap.py
-FINAL_OUTPUT_VCF=/data/users/kishwar/friday_outputs/vcf_output/pfda_friday_HG002_GRCh37.vcf.gz
-TRUTH_VCF=/data/users/common/vcf/HG002_GRCh37.vcf.gz
-CONFIDENT_BED=/data/users/common/bed/HG002_GRCh37.bed
-HAPPY_OUTPUT=/data/users/kishwar/deepvariant_outputs/happy_output/pfda_hg002_grch37
-mkdir -p ${HAPPY_OUTPUT}
-
-time sudo docker run -it -v /data/:/data/ \
-  pkrusche/hap.py /opt/hap.py/bin/hap.py \
-  ${TRUTH_VCF} \
-  ${FINAL_OUTPUT_VCF} \
-  -f ${CONFIDENT_BED} \
-  -r ${REF} \
-  -o ${HAPPY_OUTPUT}/friday_hg002_grch37 \
-  --engine=vcfeval \
-  --threads=${N_SHARDS}
-wait;
